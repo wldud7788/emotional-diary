ee7154eea89a49cdb0503acfa42a96786c3629e3
이지영 <ijiyeong@ijiyeongui-MacBook-Air.local>
2025-01-13 01:49:47 +0900

    add: AppRouter 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>App</>;
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
